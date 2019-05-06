59ffdd255024ca909ad4adf381b19b534a7a0a8f
Gonzalo Escandarani <gonziesc@gmail.com>
2019-05-06 18:39:50 -0300

    starting graphql apollo server structure

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
-const express = require('express'),
-  bodyParser = require('body-parser'),
-  morgan = require('morgan'),
-  path = require('path'),
-  cors = require('cors'),
-  config = require('./config'),
-  routes = require('./app/routes'),
-  errors = require('./app/middlewares/errors'),
-  DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10,
-  DEFAULT_PARAMETER_LIMIT = 10000;
+const { ApolloServer, gql } = require('apollo-server'),
+  // queries = require('./app/queries'),
+  resolvers = require('./app/resolvers');
 
-const bodyParserJsonConfig = () => ({
-  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
-  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
-
-const bodyParserUrlencodedConfig = () => ({
-  extended: true,
-  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
-  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
-});
-
-const app = express();
-
-app.use(cors());
-
-app.use('/docs', express.static(path.join(__dirname, 'docs')));
-
-// Client must send "Content-Type: application/json" header
-app.use(bodyParser.json(bodyParserJsonConfig()));
-app.use(bodyParser.urlencoded(bodyParserUrlencodedConfig()));
-
-if (!config.isTesting) {
-  morgan.token('req-params', req => req.params);
-  app.use(
-    morgan(
-      '[:date[clf]] :remote-addr - Request ":method :url" with params: :req-params. Response status: :status.'
-    )
-  );
-}
-
-routes.init(app);
-
-app.use(errors.handle);
-
-module.exports = app;
+module.exports = server;
--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const app = require('./app'),
-  Rollbar = require('rollbar'),
-  migrationsManager = require('./migrations'),
+const server = require('./app'),
+  // migrationsManager = require('./migrations'),
   config = require('./config'),
   logger = require('./app/logger');
 
 const port = config.common.api.port || 8080;
 
-Promise.resolve()
-  .then(() => migrationsManager.check())
-  .then(() => {
-    const rollbar = new Rollbar({
+/* migrationsManager
+  .check()
+  .then(() =>
+    /* const rollbar = new Rollbar({
       accessToken: config.common.rollbar.accessToken,
       enabled: !!config.common.rollbar.accessToken,
       environment: config.common.rollbar.environment || config.environment
-    });
-    app.use(rollbar.errorHandler());
-
-    app.listen(port);
-
-    logger.info(`Listening on port: ${port}`);
+    }); */
+server
+  .listen(port)
+  .then(({ url }) => {
+    logger.info(`🚀 Server ready at ${url}`);
   })
   .catch(logger.error);
