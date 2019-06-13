ab0a158ff677fe7a88f5b98640bda33e9c4fe1fd
mpurica <maykol.purica@wolox.cl>
2019-06-12 22:59:06 -0400

    Deleting unneeded line

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,10 +2,8 @@ const { createTestClient } = require('apollo-server-testing'),
   { ApolloServer } = require('apollo-server'),
   schema = require('../app/graphql');
 
-const { query: _query, mutate: _mutate } = createTestClient(new ApolloServer({ schema }));
+const { query: _query, mutate } = createTestClient(new ApolloServer({ schema }));
 
 const query = params => _query({ query: params });
-
-const mutate = (mutation, variables) => _mutate({ mutation, variables });
 
 module.exports = { query, mutate };
