cf86e50f2b780cb96efbbc35067a071c543bf3d4
Daniel Benavides <daniel.benavides@wolox.co>
2019-08-02 13:31:05 -0500

    Added scripts file

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -1,25 +1,20 @@
 const { makeExecutableSchema } = require('graphql-tools'),
   { applyMiddleware } = require('graphql-middleware'),
   fs = require('fs'),
-  path = require('path');
+  path = require('path'),
+  { generateIndex, destructureModules } = require('./scripts');
+
+const INDEX_FILE = 'index.js',
+  SCRIPTS_FILE = 'scripts.js';
 
 const importEverything = () =>
   fs.readdirSync(__dirname, { withFileTypes: true }).reduce(
     (imports, dirent) => {
       if (dirent.isDirectory()) {
-        const index = fs.readdirSync(path.join(__dirname, dirent.name)).reduce(
-          (singleModule, file) => {
-            const fileName = file.replace(/.js/gi, '');
-            const definitions = require(path.join(__dirname, dirent.name, fileName));
-            singleModule[fileName] = definitions[fileName] ? definitions[fileName] : definitions;
-            if (definitions.schema) {
-              singleModule.schemas.push(definitions.schema);
-            }
-            return singleModule;
-          },
-          { schemas: [] }
-        );
+        const index = generateIndex(dirent);
         imports.modules.push(index);
+        return imports;
+      } else if (dirent.name === INDEX_FILE || dirent.name === SCRIPTS_FILE) {
         return imports;
       }
       imports[dirent.name.replace(/.js/gi, '')] = require(path.join(__dirname, dirent.name));
@@ -30,55 +25,7 @@ const importEverything = () =>
 
 const { types, inputs, enums, modules } = importEverything();
 
-const destructureModules = () => {
-  const rootObject = { Query: {}, Mutation: {}, Subscription: {} };
-  return modules.reduce(
-    (destructuredModules, currentModule) => {
-      destructuredModules.schemas.push(...currentModule.schemas);
-
-      const { fieldResolvers } = currentModule.resolvers ? currentModule.resolvers : {};
-      destructuredModules.resolvers = {
-        ...destructuredModules.resolvers,
-        Query: {
-          ...destructuredModules.resolvers.Query,
-          ...currentModule.queries
-        },
-        Mutation: {
-          ...destructuredModules.resolvers.Mutation,
-          ...currentModule.mutations
-        },
-        Subscription: {
-          ...destructuredModules.resolvers.Subscription,
-          ...currentModule.subscriptions
-        },
-        ...fieldResolvers
-      };
-
-      if (currentModule.middlewares) {
-        destructuredModules.middlewares = {
-          ...destructuredModules.middlewares,
-          Query: {
-            ...destructuredModules.middlewares.Query,
-            ...currentModule.middlewares.queries
-          },
-          Mutation: {
-            ...destructuredModules.middlewares.Mutation,
-            ...currentModule.middlewares.mutations
-          },
-          Subscription: {
-            ...destructuredModules.middlewares.Subscription,
-            ...currentModule.middlewares.subscriptions
-          },
-          ...currentModule.middlewares.fieldMiddlewares
-        };
-      }
-      return destructuredModules;
-    },
-    { schemas: [], resolvers: rootObject, middlewares: rootObject }
-  );
-};
-
-const destructuredModules = destructureModules();
+const destructuredModules = destructureModules(modules);
 
 const schema = makeExecutableSchema({
   typeDefs: [types, inputs, ...destructuredModules.schemas],
