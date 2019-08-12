79a38d060b1eac4da5caecb4ebd903e753c6f3dd
Daniel Benavides <daniel.benavides@wolox.co>
2019-08-12 17:59:25 -0500

    schema.js file now can also be called type_defs.js

--- app/graphql/scripts.js
+++ app/graphql/scripts.js
@@ -3,16 +3,17 @@ const fs = require('fs'),
   merge = require('lodash.merge');
 
 const SCHEMA = 'schema';
+const TYPE_DEFS = 'type_defs';
 const RESOLVERS = 'resolvers';
 const MIDDLEWARES = 'middlewares';
 
-exports.importEverything = () =>
+exports.importModules = () =>
   fs.readdirSync(__dirname, { withFileTypes: true }).reduce(
     (imports, dirent) => {
       if (dirent.isDirectory()) {
         fs.readdirSync(path.join(__dirname, dirent.name)).forEach(file => {
           const fileName = file.replace(/.js/gi, '');
-          if (fileName === SCHEMA) {
+          if (fileName === SCHEMA || fileName === TYPE_DEFS) {
             const { typeDefs } = require(path.join(__dirname, dirent.name, fileName));
             imports.typeDefs.push(...typeDefs);
           }
